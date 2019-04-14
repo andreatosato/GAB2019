b596f13960731bbc2a761feab64ccc1c2cd217bb
Andrea Tosato <andrea.tosato@hotmail.it>
2019-04-14 09:50:06 +0200

    Diagrams

--- AzureFunctionDeepDive/DeepDive.Extension.SQLBinding/Output/SqlOutputAsyncConverterBuilder.cs
+++ AzureFunctionDeepDive/DeepDive.Extension.SQLBinding/Output/SqlOutputAsyncConverterBuilder.cs
@@ -1,17 +1,17 @@
-﻿using Microsoft.Azure.WebJobs;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿//using Microsoft.Azure.WebJobs;
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Threading;
+//using System.Threading.Tasks;
 
-namespace DeepDive.Extension.SQLBinding
-{
-    public class SqlOutputAsyncConverterBuilder<T> : IAsyncConverter<SqlOutputBindingAttribute, SqlOutputAsyncCollector<T>> where T : class
-    {
-        public Task<SqlOutputAsyncCollector<T>> ConvertAsync(SqlOutputBindingAttribute input, CancellationToken cancellationToken)
-        {
-            return Task.FromResult(new SqlOutputAsyncCollector<T>(input));
-        }
-    }
-}
+//namespace DeepDive.Extension.SQLBinding
+//{
+//    public class SqlOutputAsyncConverterBuilder<T> : IAsyncConverter<SqlOutputBindingAttribute, SqlOutputAsyncCollector<T>> where T : class
+//    {
+//        public Task<SqlOutputAsyncCollector<T>> ConvertAsync(SqlOutputBindingAttribute input, CancellationToken cancellationToken)
+//        {
+//            return Task.FromResult(new SqlOutputAsyncCollector<T>(input));
+//        }
+//    }
+//}
